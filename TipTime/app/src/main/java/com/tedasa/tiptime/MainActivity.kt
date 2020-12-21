4dc78cdfe4e84a1e807841cc206edc7f4085a1d3
Sulaiman <sulaiman_666@apps.ipb.ac.id>
2020-12-21 20:26:06 +0700

    Calculate the tip
    Giving tip logic to UI

--- TipTime/app/src/main/java/com/tedasa/tiptime/MainActivity.kt
+++ TipTime/app/src/main/java/com/tedasa/tiptime/MainActivity.kt
@@ -1,11 +1,41 @@
 package com.tedasa.tiptime
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.tedasa.tiptime.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.calculateButton.setOnClickListener { calculateTip() }
+    }
+
+    private fun calculateTip() {
+        val stringInTextField = binding.costOfService.text.toString()
+        val cost = stringInTextField.toDoubleOrNull()
+        if (cost == null) {
+            displayTip(0.0)
+            return
+        }
+        val tipPercentage = when(binding.tipOption.checkedRadioButtonId){
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            else -> 0.15
+        }
+        var tip = tipPercentage * cost
+        if (binding.roundupSwitch.isChecked){
+            tip = kotlin.math.ceil(tip)
+        }
+        displayTip(tip)
+    }
+
+    private fun displayTip(tip : Double) {
+        val formattedTip = java.text.NumberFormat.getCurrencyInstance().format(tip)
+        binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
     }
 }
