e0abddc3144b2988465a969344c30a63b7329697
TheCrimson <sulaimaniman6@gmail.com>
2021-02-24 23:09:09 +0700

    About Me App Data Binding

--- about-me-app/app/src/main/java/com/example/aboutmeapp/MainActivity.kt
+++ about-me-app/app/src/main/java/com/example/aboutmeapp/MainActivity.kt
@@ -8,26 +8,34 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutmeapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 //        findViewById<Button>(R.id.done_button).setOnClickListener {
 //            addNickName(it)
 //        }
     }
 
     fun addNickName(view: View) {
-        val editText = findViewById<EditText>(R.id.nickname_edit)
-        val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        //val editText = findViewById<EditText>(R.id.nickname_edit)
+        val editText = binding.nicknameEdit
+        //val nicknameTextView = findViewById<TextView>(R.id.nickname_text)
+        val nicknameTextView = binding.nicknameText
         val inputMethodManager = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         editText.requestFocus()
         inputMethodManager.showSoftInput(editText, 0)
-        nicknameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameTextView.visibility = View.VISIBLE
+        binding.apply {
+            nicknameTextView.text = editText.text
+            editText.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameTextView.visibility = View.VISIBLE
+        }
         inputMethodManager.hideSoftInputFromWindow(view.windowToken, 0)
     }
 
