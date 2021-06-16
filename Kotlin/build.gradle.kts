c6d7151525fab35a7ca5b023ecbc99c210b7eb52
TheCrimson <sulaimaniman6@gmail.com>
2021-06-16 14:38:03 +0700

    update gradle

--- Kotlin/build.gradle.kts
+++ Kotlin/build.gradle.kts
@@ -11,7 +11,16 @@ repositories {
 }
 dependencies {
     testImplementation(kotlin("test-junit"))
+    implementation(kotlin("stdlib-jdk8"))
 }
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
+}
+val compileKotlin: KotlinCompile by tasks
+compileKotlin.kotlinOptions {
+    jvmTarget = "1.8"
+}
+val compileTestKotlin: KotlinCompile by tasks
+compileTestKotlin.kotlinOptions {
+    jvmTarget = "1.8"
 }
